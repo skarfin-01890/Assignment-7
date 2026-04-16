853d1c2599b3384e79b8c9a80612ce8a9348c6e9
skarfin-6568 <skarfin6568@gmail.com>
2026-04-17 00:03:38 +0600

    navbar complete

---
+++ src/Components/NavBar.jsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { FaHome } from 'react-icons/fa';
+import { ImStatsDots } from 'react-icons/im';
+import { RiTimeLine } from 'react-icons/ri';
+import { NavLink } from 'react-router';
+
+const NavBar = () => {
+	return (
+
+
+
+<div className="navbar bg-base-100 shadow-sm flex justify-between px-15">
+  <div className="">
+    <a className=" text-4xl font-extrabold">Keen<span className='text-[#244D3F]'>Keeper</span></a>
+  </div>
+  <div className="flex-none">
+
+          <ul className="bg-base-100 space-x-4 flex">
+            <NavLink to={'/'} className={  ({isActive})=>{
+			return` text-white px-3 flex justify-center items-center ${isActive?"bg-[#244D3F]  text-white p-3":"bg-white text-black p-3"}
+			 ` }}><FaHome />Home</NavLink>
+             <NavLink to={'/time'}className={  ({isActive})=>{
+			return` flex gap-1 items-center ${isActive?"bg-[#244D3F] text-white p-3":"bg-white text-black p-3"} ` }}><RiTimeLine />Timeline</NavLink>
+
+             <NavLink to={'/stats'}className={  ({isActive})=>{
+			return` gap-1  flex  items-center ${isActive?"bg-[#244D3F]  text-white p-3":"bg-white text-black p-3"} ` }}><ImStatsDots />Stats</NavLink>
+
+          </ul>
+
+  </div>
+</div>
+
+
+
+	);
+};
+
+export default NavBar;
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
+import { Component, StrictMode } from 'react'
+
 import './index.css'
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
+import { createBrowserRouter } from "react-router";
+import { RouterProvider } from "react-router/dom";
+import HomePage from './pages/HomePage';
+import TimeLine from './pages/TimeLine.jsx';
+import Stats from './pages/Stats.jsx';
+
+
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App></App>,
+	children:[
+		{
+
+			index:"true",
+			Component:HomePage
+		},
+		{
+			path:'/time',
+			Component:TimeLine},
+			{
+path:"/stats",
+Component:Stats
+
+			},
+
+
+	]
+	}
+,
+]);
+
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-
+<RouterProvider router={router} />,
   </StrictMode>,
 )
